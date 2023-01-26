786f007469f711ad6b8777a43c1eeaa1a8a9e3cd
anekha sokhal <anekha17@yahoo.com>
2023-01-26 19:58:40 +0400

    Code improved

--- currencies.py
+++ currencies.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-docstring
 
-# TODO: add some currency rates
 RATES = {"USDEUR": 0.85, "GBPEUR":1.13, "CHFEUR":0.86, "EURGBP": 0.885}
 
 def convert(amount, currency):
@@ -10,5 +9,4 @@ def convert(amount, currency):
     """
     if amount[1]+currency in RATES.keys():
         return round(RATES[amount[1]+currency]*amount[0],0)
-    else:
-        return None
+    return None
